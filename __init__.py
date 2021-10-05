61710ca31e7315ed1e0d0ff3587395f529feaf02
CaelumTerrae <gavinlifrieri@gmail.com>
2021-10-05 14:47:33 -0400

    initial stufF

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
-print("hello world")
+import requests
+
+headers = {
+    'X-API-KEY': 'test key goes here'
+}
+
+r = requests.get(url='https://api.tikapi.io/public/explore?count=5',
+                 headers=headers)
+
+print(r.json())
